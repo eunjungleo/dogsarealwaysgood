376458757b37b4f649d257fb543a5a4301bd9b7b
eunjungleo <asaprocky123@likelion.org>
2020-06-27 18:26:00 +0900

    ready to deploy

--- youtube.py
+++ youtube.py
@@ -20,23 +20,17 @@ while num_of_pagedowns:
 # get url
 get_url = driver.find_elements_by_class_name('yt-simple-endpoint.style-scope.ytd-playlist-video-renderer')
 
-
-for url in get_url:
-    url = url.get_attribute('href')
-    print(url)
-
 get_title = driver.find_elements_by_css_selector('#video-title')
 
 
 f = open('youtube.csv', mode='w', encoding='utf-8', newline='')
 writer = csv.writer(f)
-writer.writerow(["url", "y_title"])
-for u in get_url:
-    url = u.get_attribute("href")
-for t in get_title:
-    y_title = t.text
-    join_title = y_title.replace("세상에 나쁜 개는 없다 -",'').replace(" ","")[:5]
-    writer.writerow([url, y_title, join_title])
+writer.writerow(["url", "y_title", "join_title"])
+for list in zip(get_url, get_title):
+    url = list[0].get_attribute("href")
+    title = list[1].text
+    join_title = join_title = title.replace("세상에 나쁜 개는 없다 -",'').replace(" ","")[:5]
+    writer.writerow([url, title, join_title])
 
 
 driver.quit()
